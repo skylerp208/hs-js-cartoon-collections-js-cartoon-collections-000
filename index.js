4f8c09d4c5bee34c0f237b3894a0fe35d7b26ad5
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-09-13 03:26:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+var dwarves = ['Dwalin', 'Balin', 'Fili', 'Kili']
 function dwarfRollCall(dwarves) {
+  var newArray = []
+  for( let i = 0; i < dwarves.length; i++) {
+    newArray.push( i+1 + '. ' + dwarves[i] + ' ' )
+  }
+  return newArray.join('')
 }
 
+var planeteerCalls = ['earth', 'wind', 'fire']
 function summonCaptainPlanet(planeteerCalls){
+  var newArray = []
+  for(let i =0; i <planeteerCalls.length; i++) {
+    newArray.push(planeteerCalls[i].toUpperCase() + '!')
+  }
+  return newArray
 }
 
+var words = ['earth' , 'fire']
 function longPlaneteerCalls(words) {
+  for (let i =0; i<words.length ; i++) {
+    if (words[i].length<5) {
+      return false
+    } else {
+      return true
+    }
+  }
 }
 
+var foods = ['pasta' , 'cheddar', 'acorns']
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === 'cheddar') {
+      return 'cheddar'
+    } else  if (foods[i] === 'gouda') { 
+      return 'gouda'
+  } else if (foods[i] === 'camembert') {
+    return 'camembert'
+  } else {
+  }
+  }
+  return 'no cheese!'
 }
